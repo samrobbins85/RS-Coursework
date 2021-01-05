27d2f3e5310dceca8f6e1b43b0151cbb5ac213dd
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-05 15:20:00 +0000

    Tidy up, next is to get more details about the business

--- user_average.py
+++ user_average.py
@@ -36,10 +36,22 @@ with open("output_date_short.json") as infile:
         .reset_index()
         .drop(columns=["counts", "user_counts"])
     )
-    print(user_scores)
 
+
+avg_stars = df.drop(columns=["review_id", "user_id"]).groupby(["business_id"]).mean()
 
 business_sort_occurrence = business_sort_occurrence.drop(columns=["counts"])
+business_sort_occurrence = pd.merge(
+    business_sort_occurrence,
+    avg_stars,
+    on="business_id",
+)
+
+avg_stars = avg_stars.rename(columns={"stars": "avg_stars"})
+
+user_scores = pd.merge(user_scores, avg_stars, on="business_id")
+
+
 for count, business in enumerate(user_scores["business_id"]):
     cossim = []
     for count1, business1 in enumerate(business_sort_occurrence["business_id"]):
@@ -72,23 +84,35 @@ for ind in business_sort_occurrence.index:
         math.isnan(business_sort_occurrence[i][ind]) for i in user_scores["business_id"]
     )
     if all(nan):
-        print(ind)
         business_sort_occurrence = business_sort_occurrence.drop([ind])
 scores = []
+adj_scores = []
+# This generates basic scores, only gives meaningful results if the user has reviewed more than 1 restaurant
 for ind in business_sort_occurrence.index:
     top = 0
     bottom = 0
+    adj_top = 0
     for i in user_scores.index:
         if (
-            math.isnan(business_sort_occurrence[user_scores["business_id"][i]][ind])
+            math.isnan(
+                business_sort_occurrence.at[ind, user_scores.at[i, "business_id"]]
+            )
             == False
         ):
-            bottom += business_sort_occurrence[user_scores["business_id"][i]][ind]
+            bottom += business_sort_occurrence.at[ind, user_scores.at[i, "business_id"]]
             top += (
-                business_sort_occurrence[user_scores["business_id"][i]][ind]
-                * user_scores["stars"][i]
+                business_sort_occurrence.at[ind, user_scores.at[i, "business_id"]]
+                * user_scores.at[i, "stars"]
             )
-    scores.append(top / bottom)
-business_sort_occurrence["scores"] = scores
+            adj_top += business_sort_occurrence[user_scores["business_id"][i]][ind] * (
+                user_scores["stars"][i] - user_scores["avg_stars"][i]
+            )
 
-print(business_sort_occurrence)
+    scores.append(top / bottom)
+    adj_scores.append(business_sort_occurrence["stars"][ind] + (adj_top / bottom))
+business_sort_occurrence["weighted_average"] = scores
+business_sort_occurrence["adjusted_weighted_average"] = adj_scores
+result = business_sort_occurrence[
+    ["business_id", "weighted_average", "adjusted_weighted_average"]
+]
+print(result)
