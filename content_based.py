c9869615388e20c64950cee92d237ba8783b13b7
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-08 14:39:40 +0000

    Merge content based filtering in

--- content_based.py
+++ content_based.py
@@ -4,25 +4,24 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
 
-with open("output_desc.json", "r") as infile:
-    data = json.load(infile)
-    df = pd.DataFrame.from_dict(data)
-    tf = TfidfVectorizer(
-        analyzer="word", ngram_range=(1, 1), min_df=20, stop_words="english"
-    )
-    tfidf_matrix = tf.fit_transform(df["text"])
-    unsparse = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix)
-    small = unsparse.iloc[
-        df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist(), :
-    ]
-    reduced_df = df[["business_id"]].reset_index()
-    cosine_similarities = linear_kernel(tfidf_matrix, small)
-    panda_cos = pd.DataFrame(cosine_similarities).reset_index()
-    merge = (
-        pd.merge(reduced_df, panda_cos, on="index")
-        .drop(columns=["index"])
-        .drop(df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist())
-    )
-    merge["mean"] = merge.mean(axis=1)
-    merge = merge[["business_id", "mean"]].groupby("business_id").mean()
-    print(merge.sort_values(by=["mean"], ascending=False))
+
+infile = open("output_desc.json", "r")
+data = json.load(infile)
+df = pd.DataFrame.from_dict(data)
+tf = TfidfVectorizer(
+    analyzer="word", ngram_range=(1, 1), min_df=0, stop_words="english"
+)
+tfidf_matrix = tf.fit_transform(df["text"])
+unsparse = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix)
+small = unsparse.iloc[df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist(), :]
+reduced_df = df[["business_id"]].reset_index()
+cosine_similarities = linear_kernel(tfidf_matrix, small)
+panda_cos = pd.DataFrame(cosine_similarities).reset_index()
+merge = (
+    pd.merge(reduced_df, panda_cos, on="index")
+    .drop(columns=["index"])
+    .drop(df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist())
+)
+merge["mean"] = merge.mean(axis=1)
+merge = merge[["business_id", "mean"]].groupby("business_id").mean()
+print(merge.sort_values(by=["mean"], ascending=False))
--- user_average.py
+++ user_average.py
@@ -5,6 +5,33 @@ from scipy.spatial.distance import cosine
 import math
 import numpy as np
 from prompt_toolkit.shortcuts import radiolist_dialog
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+
+def content_based():
+    infile = open("output_desc.json", "r")
+    data = json.load(infile)
+    df = pd.DataFrame.from_dict(data)
+    tf = TfidfVectorizer(
+        analyzer="word", ngram_range=(1, 1), min_df=0, stop_words="english"
+    )
+    tfidf_matrix = tf.fit_transform(df["text"])
+    unsparse = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix)
+    small = unsparse.iloc[
+        df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist(), :
+    ]
+    reduced_df = df[["business_id"]].reset_index()
+    cosine_similarities = linear_kernel(tfidf_matrix, small)
+    panda_cos = pd.DataFrame(cosine_similarities).reset_index()
+    merge = (
+        pd.merge(reduced_df, panda_cos, on="index")
+        .drop(columns=["index"])
+        .drop(df.index[df["user_id"] == "BdRMVROS1MXOHxr-bdZv0g"].tolist())
+    )
+    merge["mean"] = merge.mean(axis=1)
+    merge = merge[["business_id", "mean"]].groupby("business_id").mean()
+    return merge.nlargest(300, "mean")
 
 
 with open("output_date_short.json") as infile:
@@ -15,12 +42,14 @@ with open("output_date_short.json") as infile:
         df.groupby(["business_id"]).size().reset_index(name="counts")
     )
     user_occurrence = df.groupby(["user_id"]).size().reset_index(name="user_counts")
-    business_sort_occurrence = business_sort_occurrence.nlargest(50, "counts")
+    business_sort_occurrence = business_sort_occurrence.nlargest(300, "counts")
+    business_sort_occurrence = pd.merge(
+        business_sort_occurrence, content_based(), on="business_id"
+    )
     merge = pd.merge(df, business_sort_occurrence, on="business_id")
     merge = pd.merge(merge, user_occurrence, on="user_id")
     merge = merge.sort_values(by=["counts"], ascending=False)
-    users = merge.nlargest(250, "user_counts").groupby("user_id").mean()
-    # This will give a random sample of 10 of the 500 users who review italian restaurants most
+    users = merge.nlargest(100, "user_counts").groupby("user_id").mean()
     sample = users.sample(n=10).reset_index()
     mylist = sample["user_id"].tolist()
     mylist = list(map(lambda x: (x, x), mylist))
