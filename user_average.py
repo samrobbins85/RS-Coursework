fe2f45ed6190924a1ff2221d825e285682d42838
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-11 15:50:52 +0000

    Write up one evaluation method

--- user_average.py
+++ user_average.py
@@ -7,7 +7,7 @@ import numpy as np
 from prompt_toolkit.shortcuts import radiolist_dialog
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
 
 
 def content_based(user_id, toremove):
@@ -134,91 +134,158 @@ users = users.nlargest(10, "user_counts")
 sample = users.sample(n=10).reset_index()
 # Turn this dataframe to a list to display to the user
 mylist = sample["user_id"].tolist()
-mylist = list(map(lambda x: (x, x), mylist))
-chosen_user = radiolist_dialog(
-    title="User selection",
-    text="Which user would you like to choose",
-    values=mylist,
-).run()
-# Sample half of the businesses the user has reviewed to hide for evaluation
-toremove = (
-    merge.loc[merge["user_id"] == chosen_user]
-    .groupby(["business_id"])
-    .mean()
-    .reset_index()["business_id"]
-    .sample(frac=0.5)
-    .tolist()
-)
-eval_user_scores = (
-    merge.loc[merge["user_id"] == chosen_user]
-    .groupby(["business_id"])
-    .mean()
-    .reset_index()
-    .drop(columns=["counts", "user_counts"])
-)
-# From merge and df, remove the reviews talked about before
-indexNames = merge[
-    (merge["user_id"] == chosen_user) & (merge["business_id"].isin(toremove))
-].index
-merge.drop(indexNames, inplace=True)
-dfindex = df[(df["user_id"] == chosen_user) & (df["business_id"].isin(toremove))].index
-df.drop(dfindex, inplace=True)
-# For the specified user, now get their ratings of restaurants
-user_scores = (
-    merge.loc[merge["user_id"] == chosen_user]
-    .groupby(["business_id"])
-    .mean()
-    .reset_index()
-    .drop(columns=["counts", "user_counts"])
-)
-# Average rating of each restaurant
-avg_stars = df.drop(columns=["review_id", "user_id"]).groupby(["business_id"]).mean()
-# Now make business sort occurence feature those stars
-business_sort_occurrence = business_sort_occurrence.drop(columns=["counts"])
-business_sort_occurrence = pd.merge(
-    business_sort_occurrence,
-    avg_stars,
-    on="business_id",
-)
-# Change the name so it merges nicely
-avg_stars = avg_stars.rename(columns={"stars": "avg_stars"})
-# For the places the user has rated, also have the average rating of that place
-user_scores = pd.merge(user_scores, avg_stars, on="business_id")
-scores = collaborative(business_sort_occurrence, user_scores, df)
-content = content_based(chosen_user, toremove)
-scores = pd.merge(scores, content, on="business_id", how="inner")
-scores["adjusted_weighted_average"] = (
-    scores["adjusted_weighted_average"] - scores["adjusted_weighted_average"].min()
-) / (
-    scores["adjusted_weighted_average"].max()
-    - scores["adjusted_weighted_average"].min()
-)
-scores["mean"] = (scores["mean"] - scores["mean"].min()) / (
-    scores["mean"].max() - scores["mean"].min()
-)
-scores["merge"] = scores["mean"] + scores["adjusted_weighted_average"]
-# covid_data = []
-# for line in open("yelp_academic_dataset_covid_features.json", "r"):
-#     myline = json.loads(line)
-#     covid_data.append(myline)
-# covid_df = pd.DataFrame.from_dict(covid_data).dropna(how="all")
-# scores = pd.merge(scores, covid_df, on="business_id")
-# collated = scores[scores["Temporary Closed Until"] == "FALSE"]
-best = scores.nlargest(20, "merge")
-infile = open("output_business_names.json")
-data = json.load(infile)
-business_names = pd.DataFrame.from_dict(data)
-collated = pd.merge(best, business_names, on="business_id")
-user = []
-sugg = []
-print(collated)
-for index, row in collated.iterrows():
-    # if row["Covid Banner"] != "FALSE":
-    #     banner = " - " + row["Covid Banner"]
-    # else:
-    #     banner = ""]
-    if row["business_id"] in toremove:
-        print(row["business_id"])
-eval_user_scores = eval_user_scores[eval_user_scores["business_id"].isin(toremove)]
-evaluate = pd.merge(eval_user_scores, scores, on="business_id", how="inner")
-print(evaluate.sort_values(by=["merge"]))
+# mylist = list(map(lambda x: (x, x), mylist))
+# chosen_user = radiolist_dialog(
+#     title="User selection",
+#     text="Which user would you like to choose",
+#     values=mylist,
+# ).run()
+sum_tn = 0
+sum_fp = 0
+sum_fn = 0
+sum_tp = 0
+for chosen_user in tqdm(mylist):
+    with open("output_date_short.json") as infile:
+        data = json.load(infile)
+    df = pd.DataFrame.from_dict(data)
+    df = df.drop(columns=["useful", "funny", "cool", "date"])
+    # How many reviews a user has left
+    user_occurrence = (
+        df.groupby(["user_id", "business_id"])
+        .mean()
+        .groupby(["user_id"])
+        .size()
+        .reset_index(name="user_counts")
+    )
+    # Users that just leave one review
+    one_users = user_occurrence.loc[user_occurrence["user_counts"] == 1]
+    # Remove from dataframe
+    df = df[~df["user_id"].isin(one_users["user_id"].tolist())]
+    # Group by the business, and get a count of how many reviews they have
+    business_sort_occurrence = (
+        df.groupby(["business_id"]).size().reset_index(name="counts")
+    )
+    # Add this counts column to merge
+    merge = pd.merge(df, business_sort_occurrence, on="business_id")
+    # Also get the user counts
+    merge = pd.merge(merge, user_occurrence, on="user_id")
+    # Get the average score each user leaves
+    users = merge.groupby("user_id").mean()
+    # Choose the n users that have reviewed the most restaurants
+    users = users.nlargest(10, "user_counts")
+    # Sample half of the businesses the user has reviewed to hide for evaluation
+    toremove = (
+        merge.loc[merge["user_id"] == chosen_user]
+        .groupby(["business_id"])
+        .mean()
+        .reset_index()["business_id"]
+        .sample(frac=0.5)
+        .tolist()
+    )
+    eval_user_scores = (
+        merge.loc[merge["user_id"] == chosen_user]
+        .groupby(["business_id"])
+        .mean()
+        .reset_index()
+        .drop(columns=["counts", "user_counts"])
+    )
+    # From merge and df, remove the reviews talked about before
+    indexNames = merge[
+        (merge["user_id"] == chosen_user) & (merge["business_id"].isin(toremove))
+    ].index
+    merge.drop(indexNames, inplace=True)
+    dfindex = df[
+        (df["user_id"] == chosen_user) & (df["business_id"].isin(toremove))
+    ].index
+    df.drop(dfindex, inplace=True)
+    # For the specified user, now get their ratings of restaurants
+    user_scores = (
+        merge.loc[merge["user_id"] == chosen_user]
+        .groupby(["business_id"])
+        .mean()
+        .reset_index()
+        .drop(columns=["counts", "user_counts"])
+    )
+    # Average rating of each restaurant
+    avg_stars = (
+        df.drop(columns=["review_id", "user_id"]).groupby(["business_id"]).mean()
+    )
+    # Now make business sort occurence feature those stars
+    business_sort_occurrence = business_sort_occurrence.drop(columns=["counts"])
+    business_sort_occurrence = pd.merge(
+        business_sort_occurrence,
+        avg_stars,
+        on="business_id",
+    )
+    # Change the name so it merges nicely
+    avg_stars = avg_stars.rename(columns={"stars": "avg_stars"})
+    # For the places the user has rated, also have the average rating of that place
+    user_scores = pd.merge(user_scores, avg_stars, on="business_id")
+    scores = collaborative(business_sort_occurrence, user_scores, df)
+    content = content_based(chosen_user, toremove)
+    scores = pd.merge(scores, content, on="business_id", how="inner")
+    scores["adjusted_weighted_average"] = (
+        scores["adjusted_weighted_average"] - scores["adjusted_weighted_average"].min()
+    ) / (
+        scores["adjusted_weighted_average"].max()
+        - scores["adjusted_weighted_average"].min()
+    )
+    scores["mean"] = (scores["mean"] - scores["mean"].min()) / (
+        scores["mean"].max() - scores["mean"].min()
+    )
+    scores["merge"] = scores["mean"] + scores["adjusted_weighted_average"]
+    # scores.loc[scores["stars"] > 3, "stars"] = 1
+    # scores.loc[scores["stars"] <= 3, "stars"] = 0
+    # covid_data = []
+    # for line in open("yelp_academic_dataset_covid_features.json", "r"):
+    #     myline = json.loads(line)
+    #     covid_data.append(myline)
+    # covid_df = pd.DataFrame.from_dict(covid_data).dropna(how="all")
+    # scores = pd.merge(scores, covid_df, on="business_id")
+    # collated = scores[scores["Temporary Closed Until"] == "FALSE"]
+    best = scores.nlargest(20, "merge")
+    infile = open("output_business_names.json")
+    data = json.load(infile)
+    business_names = pd.DataFrame.from_dict(data)
+    collated = pd.merge(best, business_names, on="business_id")
+    user = []
+    sugg = []
+    # for index, row in collated.iterrows():
+    #     # if row["Covid Banner"] != "FALSE":
+    #     #     banner = " - " + row["Covid Banner"]
+    #     # else:
+    #     #     banner = ""]
+    #     if row["business_id"] in toremove:
+    #         print(row["business_id"])
+    eval_user_scores = eval_user_scores[eval_user_scores["business_id"].isin(toremove)]
+    evaluate = pd.merge(eval_user_scores, scores, on="business_id", how="inner")
+    print(evaluate)
+    evaluate.loc[evaluate["stars"] <= 3, "stars"] = 0
+    evaluate.loc[evaluate["stars"] > 3, "stars"] = 1
+    evaluate.loc[evaluate["merge"] <= 1, "merge"] = 0
+    evaluate.loc[evaluate["merge"] > 1, "merge"] = 1
+    evaluate.loc[
+        evaluate["mean"] <= 0.5, "mean"
+    ] = 0
+    evaluate.loc[
+        evaluate["mean"] > 0.5, "mean"
+    ] = 1
+    confuse = confusion_matrix(
+        evaluate["stars"].tolist(), evaluate["mean"].tolist()
+    ).ravel()
+    if len(confuse) == 4:
+        tn, fp, fn, tp = confuse
+        sum_tn += tn
+        sum_fp += fp
+        sum_fn += fn
+        sum_tp += tp
+    print("Round")
+    print(sum_tn)
+    print(sum_fp)
+    print(sum_fn)
+    print(sum_tp)
+
+print(sum_tn)
+print(sum_fp)
+print(sum_fn)
+print(sum_tp)
